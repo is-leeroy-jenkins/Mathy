693101b339f463a822fcb0dda43508032a6c7fbd
teppler <terryeppler@gmail.com>
2025-10-11 15:40:10 -0400

    updates

--- classifications.py
+++ classifications.py
@@ -53,7 +53,9 @@ import sklearn.neighbors as skn
 import sklearn.neural_network as snn
 import sklearn.svm as skv
 import sklearn.tree as skd
+from matplotlib import markers
 from matplotlib.colors import ListedColormap
+from seaborn import colors
 from sklearn.base import ClassifierMixin
 from sklearn.metrics import (accuracy_score, confusion_matrix, classification_report, r2_score,
                              mean_squared_error, mean_absolute_error,
@@ -1332,20 +1334,29 @@ class LogisticRegression( Classifier ):
 		try:
 			throw_if( 'X', X )
 			throw_if( 'y', y )
+			# setup marker generator and color map
 			markers = ('o', 's', '^', 'v', '<')
 			colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
 			cmap = ListedColormap( colors[ :len( np.unique( y ) ) ] )
+			
+			# plot the decision surface
 			x1_min, x1_max = X[ :, 0 ].min( ) - 1, X[ :, 0 ].max( ) + 1
 			x2_min, x2_max = X[ :, 1 ].min( ) - 1, X[ :, 1 ].max( ) + 1
-			xx1, xx2 = np.meshgrid( np.arange( x1_min, x1_max, resolution ), np.arange( x2_min, x2_max, resolution ) )
-			lab = self.project( np.ndarray( [ xx1.ravel( ), xx2.ravel( ) ] ).T, y )
+			xx1, xx2 = np.meshgrid( np.arange( x1_min, x1_max, resolution ),
+				np.arange( x2_min, x2_max, resolution ) )
+			lab = self.project( np.array( [ xx1.ravel( ),
+											xx2.ravel( ) ] ).T )
 			lab = lab.reshape( xx1.shape )
 			plt.contourf( xx1, xx2, lab, alpha=0.3, cmap=cmap )
 			plt.xlim( xx1.min( ), xx1.max( ) )
 			plt.ylim( xx2.min( ), xx2.max( ) )
+			
+			# plot class examples
 			for idx, cl in enumerate( np.unique( y ) ):
-				plt.scatter( x=X[ y == cl, 0 ], y=X[ y == cl, 1 ], alpha=0.8, c=colors[ idx ],
-					marker=markers[ idx ], label=f'Class {cl}', edgecolor='black' )
+				plt.scatter( x=X[ y == cl, 0 ], y=X[ y == cl, 1 ], alpha=0.8, c=colors[
+					idx ], marker=markers[ idx ], label=f'Class {cl}', edgecolor='black' )
+				
+				# plot all examples
 				if test_idx:
 					X_test, y_test = X[ test_idx, : ], y[ test_idx ]
 					plt.scatter( X_test[ :, 0 ], X_test[ :, 1 ], c='none', edgecolor='black',
