04706ecf785f99d6cc72fa68186a16fa47fb06ed
teppler <terryeppler@gmail.com>
2025-10-28 08:15:55 -0400

    updates

--- classifications.py
+++ classifications.py
@@ -5709,7 +5709,7 @@ class StackingModel( Classifier ):
 			return self.model.final_estimator_
 	
 	def split_data( self, X: np.ndarray, y: np.ndarray,
-			size: int = 0.2, random: int = 42 ) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+			size: int=0.2, random: int=42 ) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
 		'''
 
 			Purpose:
